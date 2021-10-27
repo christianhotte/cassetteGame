34646e04c61ffebe10ac33f8cdd3eab36c75b9df
Christian Hotte <christianwhotte@gmail.com>
2021-10-27 11:07:50 -0400

    Implemented Debug Buttons
    Added temporary TextMeshPro buttons for using the tape recorder (also added TextMeshPro to the project and made the bones for the recorder script)

---
+++ CassetteGame/Assets/3 Scripts/RecorderController.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RecorderController : MonoBehaviour
+{
+    //Description: Processes inputs and actions for the player's tape recorder.  Handles methods for manipulating tape objects
+
+    //Classes, Enums & Structs:
+
+    //Objects & Components:
+
+    //Settings:
+
+    //Memory Vars:
+
+
+    //RUNTIME METHODS:
+    private void Awake()
+    {
+        
+    }
+
+    //USER INPUT METHODS:
+    public void OnRecord()
+    {
+        
+    }
+    public void OnPlay()
+    {
+
+    }
+    public void OnRewind()
+    {
+
+    }
+    public void OnFastForward()
+    {
+
+    }
+    public void OnStopEject()
+    {
+
+    }
+    public void OnPause()
+    {
+
+    }
+}
