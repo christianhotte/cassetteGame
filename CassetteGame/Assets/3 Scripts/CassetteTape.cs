943d9164aee9b838a37ff2cfa7f4a9ec6ac4986c
Nalesse <nalesse@student.fitchburgstate.edu>
2021-10-27 21:13:18 -0400

    Made CassetteTape.cs
    - Made an Audio file property in the cassette tape script so it can store the audio file.
    - Made a currentTape variable in the RecorderController so you can switch between tapes
    - made the cassette tape pass itself to recorder on collision and set the current tape
    - completed the OnPlay() methode in Recorder controller
    - Replaced comment categories with regions in RecorderController
    - added a test sound file so we can test if things work
    - added the 2D sprite package because that was not in the project by default for some reason
    - added click and drag to the Cassette tape script

---
+++ CassetteGame/Assets/3 Scripts/CassetteTape.cs
@@ -0,0 +1,100 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CassetteTape : MonoBehaviour
+{
+    // This is the script for the cassette tape object, contains reference to the audio file as well as click and drag functionality 
+
+    #region Public
+    
+
+    #endregion
+
+    #region Privite
+
+    // Variables for click and drag system 
+    private Camera cam1;
+
+    private float startPosX;
+    private float startPosY;
+    private bool isBeingHeld = false;
+
+    #endregion
+
+    #region Inspector Fields
+    [field: SerializeField]
+    public AudioClip AudioFile { get; set; }
+
+    #endregion
+
+    private void Awake()
+    {
+        cam1 = Camera.main;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        MoveCassette();
+    }
+
+    #region Click And Drag
+
+    // Let's player click and drag the cassette tape around
+
+    private void MoveCassette()
+    {
+        if (isBeingHeld)
+        {
+            var mousePos = GetMousePos();
+
+            // Sets the object to the point the user is clicking
+            transform.localPosition = new Vector3(mousePos.x - startPosX, mousePos.y - startPosY, 0);
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            var mousePos = GetMousePos();
+
+            var pos = transform.localPosition;
+
+            // Get's the point the user is clicking instead of the center of the object. This avoids snapping.
+            startPosX = mousePos.x - pos.x;
+            startPosY = mousePos.y - pos.y;
+
+            isBeingHeld = true;
+        }
+    }
+
+    private void OnMouseUp()
+    {
+        isBeingHeld = false;
+    }
+
+    // Helper Methods
+    private Vector3 GetMousePos()
+    {
+        // Converts mouse position to vector 3
+        var mousePos = Input.mousePosition;
+        mousePos = cam1.ScreenToWorldPoint(mousePos);
+
+        return mousePos;
+    }
+
+    #endregion
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Recorder"))
+        {
+            // When the cassette tape is dragged on to the recorder the cassette gets passed as the current tape
+            var recorder = other.gameObject.GetComponent<RecorderController>();
+            recorder.currentTape = this;
+        }
+    }
+}
